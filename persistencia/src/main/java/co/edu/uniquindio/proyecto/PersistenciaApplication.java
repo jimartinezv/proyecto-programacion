cb1b263b848045df9d44f294d190a61bce2b8728
DESKTOP-G6C1HTQ\Homero J Simpson <jimv9200@gmail.com>
2021-10-19 21:47:32 -0500

    Se crea el proyecto con la configuracion inicial

---
+++ persistencia/src/main/java/co/edu/uniquindio/proyecto/PersistenciaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersistenciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenciaApplication.class,args);
+
+    }
+}
